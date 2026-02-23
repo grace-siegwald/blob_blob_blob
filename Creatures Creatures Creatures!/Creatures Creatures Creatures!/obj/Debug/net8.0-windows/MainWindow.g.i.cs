d13b37796bb5738cab72fd21b13c03e7e2ff9e2b
grace-siegwald <siegwald@colum.edu>
2026-02-22 21:07:32 -0600

    uhhhh not sure what this is either...

--- Creatures Creatures Creatures!/Creatures Creatures Creatures!/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ Creatures Creatures Creatures!/Creatures Creatures Creatures!/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace Creatures_Creatures_Creatures_ {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Creatures Creatures Creatures!;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Creatures Creatures Creatures!;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
